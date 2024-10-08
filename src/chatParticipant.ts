059e06391e22aba44c83d4b17b22befcce669f71
amunger <aamunger@microsoft.com>
2024-10-08 14:54:16 -0700

    use tools

--- src/chatParticipant.ts
+++ src/chatParticipant.ts
@@ -12,10 +12,20 @@ export function registerChatParticipant(context: vscode.ExtensionContext) {
 
 const llmInstructions = `
 You are helpful chat assistant that creates diagrams for the user using the mermaid syntax.
+There is a selection of tools that let you retrieve extra context for generating the diagram.
+If you aren't sure which tool is relevant, you can call multiple tools. You can call tools repeatedly to take actions or gather as much context as needed until you have completed the task fully. Don't give up unless you are sure the request cannot be fulfilled with the tools you have. 
+Don't make assumptions about the situation- gather context first, then perform the task or answer the question.
+Don't ask the user for confirmation to use tools, just use them.
 The final segment of your response should always be a valid mermaid diagram prefixed with a line containing  \`\`\`mermaid
 and suffixed with a line containing \`\`\`.
 Only ever include the \`\`\` delimiter in the two places mentioned above.
 `;
+
+interface IToolCall {
+    tool: vscode.LanguageModelToolDescription;
+    call: vscode.LanguageModelChatResponseToolCallPart;
+    result: Thenable<vscode.LanguageModelToolResult>;
+}
 
 const diagramManager = new DiagramDocument();
 
@@ -40,42 +50,95 @@ async function chatRequestHandler(request: vscode.ChatRequest, chatContext: vsco
     }
     messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
 
-    let isMermaidDiagramStreamingIn = false;
-    let mermaidDiagram = '';
+    options.tools = vscode.lm.tools.map((tool): vscode.LanguageModelChatTool => {
+        return {
+            name: tool.id,
+            description: tool.description,
+            parametersSchema: tool.parametersSchema ?? {}
+        };
+    });
 
-    const response = await model.sendRequest(messages, options, token);
+    const runWithFunctions = async (): Promise<void> => {
+        const toolCalls: IToolCall[] = [];
 
-    for await (const part of response.stream) {
-        if (part instanceof vscode.LanguageModelChatResponseTextPart) {
-            if (!isMermaidDiagramStreamingIn && part.value.includes('```')) {
-                stream.progress('Validating mermaid diagram');
-                isMermaidDiagramStreamingIn = true;
+        let isMermaidDiagramStreamingIn = false;
+        let mermaidDiagram = '';
+
+        const response = await model.sendRequest(messages, options, token);
+
+        for await (const part of response.stream) {
+            if (part instanceof vscode.LanguageModelChatResponseTextPart) {
+                if (!isMermaidDiagramStreamingIn && part.value.includes('```')) {
+                    stream.progress('Validating mermaid diagram');
+                    isMermaidDiagramStreamingIn = true;
+                }
+
+                if (isMermaidDiagramStreamingIn) {
+                    mermaidDiagram += part.value;
+                } else {
+                    stream.markdown(part.value);
+                }
+            } else if (part instanceof vscode.LanguageModelChatResponseToolCallPart) {
+                const tool = vscode.lm.tools.find(tool => tool.id === part.name);
+                if (!tool) {
+                    // BAD tool choice?
+                    throw new Error('Got invalid tool choice: ' + part.name);
+                }
+
+                let parameters: any;
+                try {
+                    parameters = JSON.parse(part.parameters);
+                } catch (err) {
+                    throw new Error(`Got invalid tool use parameters: "${part.parameters}". (${(err as Error).message})`);
+                }
+
+                // TODO support prompt-tsx here
+                const requestedContentType = 'text/plain';
+                toolCalls.push({
+                    call: part,
+                    result: vscode.lm.invokeTool(tool.id, { parameters: JSON.parse(part.parameters), toolInvocationToken: request.toolInvocationToken, requestedContentTypes: [requestedContentType] }, token),
+                    tool
+                });
             }
 
-            if (isMermaidDiagramStreamingIn) {
-                mermaidDiagram += part.value;
-            } else {
-                stream.markdown(part.value);
+            // if any tools were used, we should add them to the context and re-run the query
+            if (toolCalls.length) {
+                const assistantMsg = vscode.LanguageModelChatMessage.Assistant('');
+                assistantMsg.content2 = toolCalls.map(toolCall => new vscode.LanguageModelChatResponseToolCallPart(toolCall.tool.id, toolCall.call.toolCallId, toolCall.call.parameters));
+                messages.push(assistantMsg);
+                for (const toolCall of toolCalls) {
+                    // NOTE that the result of calling a function is a special content type of a USER-message
+                    const message = vscode.LanguageModelChatMessage.User('');
+
+                    message.content2 = [new vscode.LanguageModelChatMessageToolResultPart(toolCall.call.toolCallId, (await toolCall.result)['text/plain']!)];
+                    messages.push(message);
+                }
+
+                // IMPORTANT The prompt must end with a USER message (with no tool call)
+                messages.push(vscode.LanguageModelChatMessage.User(`Above is the result of calling the functions ${toolCalls.map(call => call.tool.id).join(', ')}. The user cannot see this result, so you should explain it to the user if referencing it in your answer.`));
+
+                // RE-enter
+                return runWithFunctions();
             }
-        } else if (part instanceof vscode.LanguageModelChatResponseToolCallPart) {
-            throw new Error('Tool calls are not supported yet.');
         }
-    }
 
-    isMermaidDiagramStreamingIn = false;
-    
-    // Validate
-    const mermaid = (await import('mermaid')).default;
-    try {
-        const trimmedDiagram = mermaidDiagram.replace(/```mermaid/, '').replace(/```/, '').trim();
-        const diagramType = await mermaid.parse(trimmedDiagram);
-        stream.progress(`Generating ${diagramType.diagramType} diagram`);
-        stream.markdown(mermaidDiagram);
-        await diagramManager.setContent(mermaidDiagram);
-    } catch (e: any) {
-        // TODO: Loop back to fix the diagram
-        stream.markdown('Please try again.');
-        // log
-        console.error(e?.message ?? e);
-    }
+        isMermaidDiagramStreamingIn = false;
+
+        // Validate
+        const mermaid = (await import('mermaid')).default;
+        try {
+            const trimmedDiagram = mermaidDiagram.replace(/```mermaid/, '').replace(/```/, '').trim();
+            const diagramType = await mermaid.parse(trimmedDiagram);
+            stream.progress(`Generating ${diagramType.diagramType} diagram`);
+            stream.markdown(mermaidDiagram);
+            await diagramManager.setContent(mermaidDiagram);
+        } catch (e: any) {
+            // TODO: Loop back to fix the diagram
+            stream.markdown('Please try again.');
+            // log
+            console.error(e?.message ?? e);
+        }
+    };
+
+    await runWithFunctions();
 };
