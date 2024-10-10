733fbc4bbe72c4b0ad9bd9329e98b0577ce9536b
amunger <aamunger@microsoft.com>
2024-10-10 10:33:27 -0700

    show as webview rather than markdown preview

--- src/diagram.ts
+++ src/diagram.ts
@@ -4,22 +4,36 @@ import * as path from 'path';
 import { logMessage } from './extension';
 
 export class Diagram {
+    private tempDir: string | undefined;
+
     constructor(private readonly _content: string) {
     }
 
     get content(): string { return this._content; }
 
+    get asSvg(): string {
+        if (!this.tempDir) {
+            this.validate();
+        }
+
+        if (!this.tempDir) {
+            throw new Error('Failed to get SVG content');
+        }
+
+        // file is written to a slightly different name, probably need to validate
+        return fs.readFileSync(`${this.tempDir}/diagram-1.svg`, 'utf8');
+    }
+
     async validate(): Promise<{ message: string, stack: string } | undefined> {
-        const tmpDir = fs.mkdtempSync(os.tmpdir());
-        logMessage(tmpDir);
+        this.tempDir = fs.mkdtempSync(os.tmpdir());
 
         // Write the diagram to a file
-        fs.writeFileSync(path.join(tmpDir, 'diagram.md'), this.content);
+        fs.writeFileSync(path.join(this.tempDir, 'diagram.md'), this.content);
         const mermaidCLIModule = await import('@mermaid-js/mermaid-cli');
         try {
             await mermaidCLIModule.run(
-                `${tmpDir}/diagram.md`,     // input
-                `${tmpDir}/diagram.svg`,    // output
+                `${this.tempDir}/diagram.md`,     // input
+                `${this.tempDir}/diagram.svg`,               // output
                 {
                     outputFormat: 'svg',
                 }
