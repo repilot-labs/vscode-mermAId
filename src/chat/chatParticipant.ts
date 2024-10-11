bf1c21aa1c356dfb6d169770332d89684cb5e8b0
amunger <aamunger@microsoft.com>
2024-10-11 11:50:03 -0700

    use tsx prompt

---
+++ src/chat/chatParticipant.ts
@@ -0,0 +1,185 @@
+import * as vscode from 'vscode';
+import { logMessage } from '../extension';
+import { Diagram } from '../diagram';
+import { DiagramEditorPanel } from '../diagramEditorPanel';
+import { renderPrompt } from '@vscode/prompt-tsx';
+import { MermaidPrompt, ToolResultMetadata } from './mermaidPrompt';
+import { ToolCallRound } from './toolMetadata';
+
+export function registerChatParticipant(context: vscode.ExtensionContext) {
+    const handler: vscode.ChatRequestHandler = chatRequestHandler;
+
+    const participant = vscode.chat.createChatParticipant('copilot-diagram.mermAId', handler);
+    participant.iconPath = new vscode.ThemeIcon('pie-chart');
+    context.subscriptions.push(participant);
+    DiagramEditorPanel.extensionUri = context.extensionUri;
+}
+
+async function chatRequestHandler(request: vscode.ChatRequest, chatContext: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken) {
+    const models = await vscode.lm.selectChatModels({
+        vendor: 'copilot',
+        family: 'gpt-4o'
+    });
+
+    const model = models[0];
+
+    const options: vscode.LanguageModelChatRequestOptions = {
+        justification: 'To collaborate on diagrams',
+    };
+
+    options.tools = vscode.lm.tools.map((tool): vscode.LanguageModelChatTool => {
+        return {
+            name: tool.id,
+            description: tool.description,
+            parametersSchema: tool.parametersSchema ?? {}
+        };
+    });
+    logMessage(`Available tools: ${options.tools.map(tool => tool.name).join(', ')}`);
+
+    let { messages, references } = await renderPrompt(
+        MermaidPrompt,
+        {
+            context: chatContext,
+            request,
+            toolCallRounds: [],
+            toolCallResults: {},
+            command: request.command
+        },
+        { modelMaxPromptTokens: model.maxInputTokens },
+        model);
+    references.forEach(ref => {
+        if (ref.anchor instanceof vscode.Uri || ref.anchor instanceof vscode.Location) {
+            stream.reference(ref.anchor);
+        }
+    });
+
+    let retries = 0;
+    const accumulatedToolResults: Record<string, vscode.LanguageModelToolResult> = {};
+    const toolCallRounds: ToolCallRound[] = [];
+    const runWithFunctions = async (): Promise<void> => {
+
+        let isMermaidDiagramStreamingIn = false;
+        let mermaidDiagram = '';
+
+        const response = await model.sendRequest(messages, options, token);
+        const toolCalls: vscode.LanguageModelToolCallPart[] = [];
+
+        let responseStr = '';
+        for await (const part of response.stream) {
+            if (part instanceof vscode.LanguageModelTextPart) {
+                if (!isMermaidDiagramStreamingIn && part.value.includes('```')) {
+                    isMermaidDiagramStreamingIn = true;
+                }
+
+                if (isMermaidDiagramStreamingIn) {
+                    mermaidDiagram += part.value;
+                } else {
+                    stream.markdown(part.value);
+                    responseStr += part.value;
+                }
+            } else if (part instanceof vscode.LanguageModelToolCallPart) {
+                part.parameters = JSON.parse(part.parameters);
+                toolCalls.push(part);
+            }
+
+
+            if (toolCalls.length) {
+                toolCallRounds.push({
+                    response: responseStr,
+                    toolCalls
+                });
+                const result = (await renderPrompt(
+                    MermaidPrompt,
+                    {
+                        context: chatContext,
+                        request,
+                        toolCallRounds,
+                        toolCallResults: accumulatedToolResults,
+                        command: request.command
+                    },
+                    { modelMaxPromptTokens: model.maxInputTokens },
+                    model));
+                messages = result.messages;
+                const toolResultMetadata = result.metadatas.getAll(ToolResultMetadata);
+                if (toolResultMetadata?.length) {
+                    toolResultMetadata.forEach(meta => accumulatedToolResults[meta.toolCallId] = meta.result);
+                }
+
+                return runWithFunctions();
+            }
+        }
+
+        logMessage(mermaidDiagram);
+        isMermaidDiagramStreamingIn = false;
+
+        // Validate
+        stream.progress('Validating mermaid diagram');
+        const diagram = new Diagram(mermaidDiagram);
+
+        const result = await diagram.generateWithValidation();
+
+        if (!result.success) {
+            if (retries++ < 1) {
+                addNestingContext(messages);
+            }
+            if (retries++ < 2) {
+                if (retries++ < 2) {
+                    stream.progress('Attempting to fix validation errors');
+                    // we might be able to reset the messages to this message only
+                    messages.push(vscode.LanguageModelChatMessage.User(`Please fix this error to make the diagram render correctly: ${result.message}. The diagram is below:\n${mermaidDiagram}`));
+                    return runWithFunctions();
+                } else {
+                    if (result.stack) {
+                        logMessage(result.stack);
+                    }
+                    stream.markdown('Failed to generate diagram from the mermaid content. Check output log for details.');
+                    stream.markdown(mermaidDiagram);
+                }
+            }
+        } else {
+            DiagramEditorPanel.createOrShow(diagram);
+        }
+    }; // done with runWithFunctions
+
+    await runWithFunctions();
+}
+
+
+function addNestingContext(messages: vscode.LanguageModelChatMessage[]) {
+    messages.push(vscode.LanguageModelChatMessage.Assistant("Remember when creating the UML diagram in Mermaid, classes are represented as flat structures," +
+        " and Mermaid does not support nested class definitions. Instead, each class must be defined separately, and relationships between them must be explicitly stated." +
+        "Use association to connect the main class to the nested class, using cardinality to denote relationships (e.g., one-to-many)." +
+        " \n example of correct syntax: \n" +
+        `
+                classDiagram
+                    class House {
+                        string address
+                        int rooms
+                        Kitchen kitchen
+                    }
+                                    
+                    class Kitchen {
+                        string appliances
+                        int size
+                    }
+                                    
+                    House "1" --> "1" Kitchen : kitchen
+                `));
+}
+
+function specifyAssociations(messages: vscode.LanguageModelChatMessage[]) {
+    messages.push(vscode.LanguageModelChatMessage.Assistant("Remember that all class associations/should be defined. In this example:"
+        +
+        `
+            classDiagram
+            class Supermarket {
+                +Registers: CashRegister[]
+            }
+            class CashRegister {
+                +process(product: Product)
+            }
+            `
+        +
+        "This Mermaid diagram is incomplete. You should have this defined like:" + `Supermarket "1" --> "*" CashRegister : has`
+    ));
+}
---
+++ src/chat/mermaidPrompt.tsx
@@ -0,0 +1,314 @@
+import {
+	AssistantMessage,
+	BasePromptElementProps,
+	contentType as promptTsxContentType,
+	PrioritizedList,
+	PromptElement,
+	PromptElementProps,
+	PromptPiece,
+	PromptSizing,
+	UserMessage,
+	PromptMetadata,
+	ToolCall,
+	Chunk,
+	ToolMessage,
+	PromptReference,
+} from '@vscode/prompt-tsx';
+import * as vscode from 'vscode';
+import { isTsxMermaidMetadata, ToolCallRound } from './toolMetadata';
+
+export interface MermaidProps extends BasePromptElementProps {
+	request: vscode.ChatRequest;
+	command: string | undefined;
+	context: vscode.ChatContext;
+	toolCallRounds: ToolCallRound[];
+	toolCallResults: Record<string, vscode.LanguageModelToolResult>;
+}
+
+export class MermaidPrompt extends PromptElement<MermaidProps, void> {
+	render(state: void, sizing: PromptSizing) {
+		return (
+			<>
+				<UserMessage>
+					Instructions: <br />
+					- You are helpful chat assistant that creates diagrams for the user
+					using the mermaid syntax. <br />
+					- If you aren't sure which tool is relevant, you can call multiple
+					tools. You can call tools repeatedly to take actions or gather as much
+					context as needed until you have completed the task fully. Don't give up
+					unless you are sure the request cannot be fulfilled with the tools you
+					have. <br />
+					- Don't make assumptions about the situation- gather context first, then
+					perform the task or answer the question. <br />
+					- Don't ask the user for confirmation to use tools, just use them. <br />
+					- If you find a symbol you want to get the definition for, like a interface 
+					implemented by a class in the context, use the provided tool <br />
+					- The final segment of your response should always be a valid mermaid diagram 
+					prefixed with a line containing  \`\`\`mermaid and suffixed with a line 
+					containing \`\`\`. <br />
+					- Nothing should follow the closing \`\`\` delimiter and the \`\`\` delimiter 
+					should only occur in the two places mentioned above.
+				</UserMessage>
+				<RequestCommand commandName={this.props.command ?? ''}></RequestCommand>
+				<History context={this.props.context} priority={10}></History>
+				<PromptReferences
+					references={this.props.request.references}
+					priority={20}
+				/>
+				<UserMessage>{this.props.request.prompt}</UserMessage>
+				<ToolCalls
+					toolCallRounds={this.props.toolCallRounds}
+					toolInvocationToken={this.props.request.toolInvocationToken}
+					toolCallResults={this.props.toolCallResults}>
+				</ToolCalls>
+			</>
+		);
+	}
+}
+
+interface ToolCallsProps extends BasePromptElementProps {
+	toolCallRounds: ToolCallRound[];
+	toolCallResults: Record<string, vscode.LanguageModelToolResult>;
+	toolInvocationToken: vscode.ChatParticipantToolToken | undefined;
+}
+
+const agentSupportedContentTypes = [promptTsxContentType, 'text/plain'];
+const dummyCancellationToken: vscode.CancellationToken = new vscode.CancellationTokenSource().token;
+
+class ToolCalls extends PromptElement<ToolCallsProps, void> {
+	async render(state: void, sizing: PromptSizing) {
+		if (!this.props.toolCallRounds.length) {
+			return undefined;
+		}
+
+		// Note- the final prompt must end with a UserMessage
+		return <>
+			{this.props.toolCallRounds.map(round => this.renderOneToolCallRound(round, sizing))}
+			<UserMessage>Above is the result of calling one or more tools. The user cannot see the results, so you should explain them to the user if referencing them in your answer.</UserMessage>
+		</>
+	}
+
+	private renderOneToolCallRound(round: ToolCallRound, sizing: PromptSizing) {
+		const assistantToolCalls: ToolCall[] = round.toolCalls.map(tc => ({ type: 'function', function: { name: tc.name, arguments: JSON.stringify(tc.parameters) }, id: tc.toolCallId }));
+		// TODO- just need to adopt prompt-tsx update in vscode-copilot
+		return (
+			<Chunk>
+				<AssistantMessage toolCalls={assistantToolCalls}>{round.response || 'placeholder'}</AssistantMessage>
+				{round.toolCalls.map(toolCall =>
+					<ToolCallElement toolCall={toolCall} toolInvocationToken={this.props.toolInvocationToken} toolCallResult={this.props.toolCallResults[toolCall.toolCallId]}></ToolCallElement>)}
+			</Chunk>);
+	}
+}
+
+interface ToolCallElementProps extends BasePromptElementProps {
+	toolCall: vscode.LanguageModelToolCallPart;
+	toolInvocationToken: vscode.ChatParticipantToolToken | undefined;
+	toolCallResult: vscode.LanguageModelToolResult | undefined;
+}
+
+class ToolCallElement extends PromptElement<ToolCallElementProps, void> {
+	async render(state: void, sizing: PromptSizing): Promise<PromptPiece | undefined> {
+		const tool = vscode.lm.tools.find(t => t.id === this.props.toolCall.name);
+		if (!tool) {
+			console.error(`Tool not found: ${this.props.toolCall.name}`);
+			return <ToolMessage toolCallId={this.props.toolCall.toolCallId}>Tool not found</ToolMessage>;
+		}
+
+		const contentType = agentSupportedContentTypes.find(type => tool.supportedContentTypes.includes(type));
+		if (!contentType) {
+			console.error(`Tool does not support any of the agent's content types: ${tool.id}`);
+			return <ToolMessage toolCallId={this.props.toolCall.toolCallId}>Tool unsupported</ToolMessage>;
+		}
+
+		const tokenOptions: vscode.LanguageModelToolInvocationOptions<unknown>['tokenOptions'] = {
+			tokenBudget: sizing.tokenBudget,
+			countTokens: async (content: string) => sizing.countTokens(content),
+		};
+
+		const toolResult = this.props.toolCallResult ??
+			await vscode.lm.invokeTool(this.props.toolCall.name, { parameters: this.props.toolCall.parameters, requestedContentTypes: [contentType], toolInvocationToken: this.props.toolInvocationToken, tokenOptions }, dummyCancellationToken);
+		const message = (
+			<ToolMessage toolCallId={this.props.toolCall.toolCallId}>
+				<meta value={new ToolResultMetadata(this.props.toolCall.toolCallId, toolResult)}></meta>
+				{contentType === 'text/plain' ?
+					toolResult[contentType] :
+					<elementJSON data={toolResult[contentType]}></elementJSON>}
+			</ToolMessage>
+		);
+		return message;
+	}
+}
+
+export class ToolResultMetadata extends PromptMetadata {
+	constructor(
+		public toolCallId: string,
+		public result: vscode.LanguageModelToolResult,
+	) {
+		super();
+	}
+}
+
+interface HistoryProps extends BasePromptElementProps {
+	priority: number;
+	context: vscode.ChatContext;
+}
+
+class History extends PromptElement<HistoryProps, void> {
+	render(state: void, sizing: PromptSizing) {
+		return (
+			<PrioritizedList priority={this.props.priority} descending={false}>
+				{this.props.context.history.map((message) => {
+					if (message instanceof vscode.ChatRequestTurn) {
+						return (
+							<>
+								{<PromptReferences references={message.references} excludeReferences={true} />}
+								<UserMessage>{message.prompt}</UserMessage>
+							</>
+						);
+					} else if (message instanceof vscode.ChatResponseTurn) {
+						const metadata = message.result.metadata;
+						if (isTsxMermaidMetadata(metadata) && metadata.toolCallsMetadata.toolCallRounds.length > 0) {
+							return <ToolCalls toolCallResults={metadata.toolCallsMetadata.toolCallResults} toolCallRounds={metadata.toolCallsMetadata.toolCallRounds} toolInvocationToken={undefined} />;
+						}
+
+						return <AssistantMessage>{chatResponseToString(message)}</AssistantMessage>;
+					}
+				})}
+			</PrioritizedList>
+		);
+	}
+}
+
+function chatResponseToString(response: vscode.ChatResponseTurn): string {
+	return response.response
+		.map((r) => {
+			if (r instanceof vscode.ChatResponseMarkdownPart) {
+				return r.value.value;
+			} else if (r instanceof vscode.ChatResponseAnchorPart) {
+				if (r.value instanceof vscode.Uri) {
+					return r.value.fsPath;
+				} else {
+					return r.value.uri.fsPath;
+				}
+			}
+
+			return '';
+		})
+		.join('');
+}
+
+interface RequestCommandProps extends BasePromptElementProps {
+	commandName: string;
+}
+
+class RequestCommand extends PromptElement<RequestCommandProps, void> {
+	render(state: void, sizing: PromptSizing) {
+		const doc = vscode.window.activeTextEditor?.document;
+		const docRef = doc ? 
+			`The file the user currently has open is: ${doc.uri.fsPath} with contents: ${doc.getText()}` : 
+			`The user does not have any files open, the root of the workspace is: ${vscode.workspace.workspaceFolders?.[0]?.uri.fsPath}`;
+		switch (this.props.commandName) {
+			case 'uml':
+				return (
+					<>
+						<AssistantMessage>
+							The user asked for a UML diagram. Include all relevant classes in the file attached as context. You must use the tool mermAId_get_symbol_definition to get definitions of symbols
+							not defined in the current context. You should call it multiple times since you will likely need to get the definitions of multiple symbols.
+							Therefore for all classes you touch, explore their related classes using mermAId_get_symbol_definition to get their definitions and add them to the diagram."
+						</AssistantMessage>
+						<AssistantMessage>{docRef}</AssistantMessage>
+						<UserMessage>
+							Remember that all class associations/should be defined! If one class has an instance of another it should be connected it it in the UML diagram.
+						</UserMessage>
+					</>
+				);
+			default:
+				return (
+					<AssistantMessage>
+						Pick an appropriate diagram type, for example: sequence, class, or flowchart.
+					</AssistantMessage>
+				);
+		}
+	}
+}
+
+interface PromptReferencesProps extends BasePromptElementProps {
+	references: ReadonlyArray<vscode.ChatPromptReference>;
+	excludeReferences?: boolean;
+}
+
+class PromptReferences extends PromptElement<PromptReferencesProps, void> {
+	render(state: void, sizing: PromptSizing): PromptPiece {
+		return (
+			<UserMessage>
+				{this.props.references.map((ref, index) => (
+					<PromptReferenceElement ref={ref} excludeReferences={this.props.excludeReferences} />
+				))}
+			</UserMessage>
+		);
+	}
+}
+
+interface PromptReferenceProps extends BasePromptElementProps {
+	ref: vscode.ChatPromptReference;
+	excludeReferences?: boolean;
+}
+
+class PromptReferenceElement extends PromptElement<PromptReferenceProps> {
+	async render(state: void, sizing: PromptSizing): Promise<PromptPiece | undefined> {
+		const value = this.props.ref.value;
+		// TODO make context a list of TextChunks so that it can be trimmed
+		if (value instanceof vscode.Uri) {
+			const fileContents = (await vscode.workspace.fs.readFile(value)).toString();
+			return (
+				<Tag name="context">
+					{!this.props.excludeReferences && <references value={[new PromptReference(value)]} />}
+					{value.fsPath}:<br />
+					``` <br />
+					{fileContents}<br />
+					```<br />
+				</Tag>
+			);
+		} else if (value instanceof vscode.Location) {
+			const rangeText = (await vscode.workspace.openTextDocument(value.uri)).getText(value.range);
+			return (
+				<Tag name="context">
+					{!this.props.excludeReferences && <references value={[new PromptReference(value)]} /> }
+					{value.uri.fsPath}:{value.range.start.line + 1}-$<br />
+					{value.range.end.line + 1}: <br />
+					```<br />
+					{rangeText}<br />
+					```
+				</Tag>
+			);
+		} else if (typeof value === 'string') {
+			return <Tag name="context">{value}</Tag>;
+		}
+	}
+}
+
+export type TagProps = PromptElementProps<{
+	name: string;
+}>;
+
+export class Tag extends PromptElement<TagProps> {
+	private static readonly _regex = /^[a-zA-Z_][\w\.\-]*$/;
+
+	render() {
+		const { name } = this.props;
+
+		if (!Tag._regex.test(name)) {
+			throw new Error(`Invalid tag name: ${this.props.name}`);
+		}
+
+		return (
+			<>
+				{'<' + name + '>'}<br />
+				<>
+					{this.props.children}<br />
+				</>
+				{'</' + name + '>'}<br />
+			</>
+		);
+	}
+}
